38774ef0b55a8bd04f63f093c522c447b7e05122
c4072047 <c4072047@18SCOMPD172>
2024-10-25 11:54:15 +0100

    Tutorial 7 init

---
+++ 7) Scene Management/Renderer.cpp
@@ -0,0 +1,68 @@
+#include "Renderer.h"
+
+#include "../nclgl/CubeBot.h"
+
+Renderer::Renderer(Window& parent)
+	: OGLRenderer(parent)
+	, root(new SceneNode())
+	, cube(Mesh::LoadFromMeshFile("OffsetCubeY.msh"))
+	, camera(new Camera())
+	, shader(new Shader("SceneVertex.glsl", "SceneFragment.glsl"))
+{
+	if (!shader->LoadSuccess() || !cube)
+		return;
+
+	projMatrix = Matrix4::Perspective(
+		1.0f, 1000.0f,
+		(float)width / (float)height,
+		45.0f
+	);
+
+	camera->setPosition(Vector3(0, 30, 175));
+
+	SceneNode* bot = new CubeBot(cube);
+	root->addChild(bot);
+
+	for (int i = 0; i < 10; i++) {
+		bot = bot->deepCopy();
+		bot->setTransform(bot->getTransform() * Matrix4::Translation(Vector3(0, 0, -100)));
+		bot->setScale(bot->getScale() * 1.2f);
+		root->addChild(bot);
+	}
+
+	init = true;
+
+	glEnable(GL_DEPTH_TEST);
+}
+
+Renderer::~Renderer()
+{
+	delete root;
+	delete cube;
+	delete camera;
+	delete shader;
+}
+
+void Renderer::UpdateScene(float dt)
+{
+	camera->update(dt);
+	viewMatrix = camera->buildViewMatrix();
+	root->update(dt);
+}
+
+void Renderer::RenderScene()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	BindShader(shader);
+	UpdateShaderMatrices();
+
+	glUniform1i(shader->getUniform("diffuseTex"), 1);
+
+	DrawNode(root);
+}
+
+void Renderer::DrawNode(SceneNode* node)
+{
+	node->draw(*this);
+}
