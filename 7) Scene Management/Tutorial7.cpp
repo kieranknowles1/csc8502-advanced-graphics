43dbd628cfb9d489b2c086a9f66154ef41eee059
c4072047 <c4072047@18SCOMPD172>
2024-10-25 13:11:40 +0100

    Add key to lock frustum

--- 7) Scene Management/Tutorial7.cpp
+++ 7) Scene Management/Tutorial7.cpp
@@ -23,6 +23,9 @@ int main() {
 		if (Window::GetKeyboard()->KeyDown(KEYBOARD_F5)) {
 			Shader::ReloadAllShaders();
 		}
+		if (Window::GetKeyboard()->KeyTriggered(KEYBOARD_1)) {
+			renderer.toggleFrustumLock();
+		}
 	}
 
 	return 0;
