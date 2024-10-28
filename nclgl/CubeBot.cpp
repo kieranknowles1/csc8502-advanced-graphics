1148e1e99569dd21341d9ea2cd49a3faf067f4d0
c4072047 <c4072047@18SCOMPD172>
2024-10-28 10:46:08 +0000

    Fix bounds of cube bot

--- nclgl/CubeBot.cpp
+++ nclgl/CubeBot.cpp
@@ -4,41 +4,46 @@ CubeBot::CubeBot(Mesh* cube) {
 	const Vector4 RED = Vector4(1, 0, 0, 1);
 	const Vector4 GREEN = Vector4(0, 1, 0, 1);
 	const Vector4 BLUE = Vector4(0, 0, 1, 1);
+	const Vector4 YELLOW = Vector4(1, 1, 0, 1);
+
+	// Bounding sphere is based on the bottom of the mesh, so we need to
+	// use 2sqrt(2) to approximately cover the whole mesh
+	float radius = 2.0f * sqrt(2);
 
 	body = new SceneNode(cube, RED);
 	body->setScale(Vector3(10, 15, 5));
 	body->setTransform(Matrix4::Translation(Vector3(0, 0, 0)));
-	body->setBoundingRadius(15);
+	body->setBoundingRadius(radius);
 	addChild(body);
 
 	head = new SceneNode(cube, GREEN);
 	head->setScale(Vector3(5, 5, 5));
 	head->setTransform(Matrix4::Translation(Vector3(0, 30, 0)));
-	head->setBoundingRadius(5);
+	head->setBoundingRadius(radius);
 	addChild(head);
 
 	leftArm = new SceneNode(cube, BLUE);
 	leftArm->setScale(Vector3(3, -18, 3));
 	leftArm->setTransform(Matrix4::Translation(Vector3(-12, 30, -1)));
-	leftArm->setBoundingRadius(18);
+	leftArm->setBoundingRadius(radius);
 	addChild(leftArm);
 
 	rightArm = new SceneNode(cube, BLUE);
 	rightArm->setScale(Vector3(3, -18, 3));
 	rightArm->setTransform(Matrix4::Translation(Vector3(12, 30, -1)));
-	rightArm->setBoundingRadius(18);
+	rightArm->setBoundingRadius(radius);
 	addChild(rightArm);
 
-	leftLeg = new SceneNode(cube, BLUE);
+	leftLeg = new SceneNode(cube, YELLOW);
 	leftLeg->setScale(Vector3(3, -17.5, 3));
 	leftLeg->setTransform(Matrix4::Translation(Vector3(-8, 0, 0)));
-	leftLeg->setBoundingRadius(17.5);
+	leftLeg->setBoundingRadius(radius);
 	addChild(leftLeg);
 
-	rightLeg = new SceneNode(cube, BLUE);
+	rightLeg = new SceneNode(cube, YELLOW);
 	rightLeg->setScale(Vector3(3, -17.5, 3));
 	rightLeg->setTransform(Matrix4::Translation(Vector3(8, 0, 0)));
-	rightLeg->setBoundingRadius(17.5);
+	rightLeg->setBoundingRadius(radius);
 	addChild(rightLeg);
 }
 
@@ -48,6 +53,7 @@ CubeBot::~CubeBot()
 
 void CubeBot::onUpdate(float dt)
 {
+	return;
 	transform = transform
 		* Matrix4::Rotation(30.0f * dt, Vector3(0, 1, 0));
 
