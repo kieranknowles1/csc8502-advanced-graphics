30d7408784097bb68925f2cfea600f316df236e5
c4072047 <c4072047@18SCOMPD172>
2024-10-30 11:00:21 +0000

    Make windows builds slightly more resilient

--- windows_is_pain.bat
+++
@@ -1,8 +0,0 @@
-@REM CMake on windows is a necessary evil, but it's still evil
-@REM this script sets up the environment for CMake to work
-
-@REM This only works on my specific setup, but windows is suffering
-SET CMAKE_PREFIX_PATH=H:\cmake
-
-cd build
-cmake ..
