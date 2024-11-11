6e40a45a96476de5e885a315b40ecc660cbf6b52
c4072047 <c4072047@18SCOMPD172>
2024-11-11 11:41:06 +0000

    Document materiel keys

--- nclgl/Materiel.h
+++ nclgl/Materiel.h
@@ -12,6 +12,12 @@ class Shader;
 class ResourceManager;
 
 struct Materiel {
+	// Load from a MeshMaterial file
+	// The following keys are supported:
+	//  - Diffuse: The path to the diffuse texture
+	//  - Bump: The path to the normal map
+	//  - Cull: The cull mode to use. Can be GL_FRONT, GL_BACK, or GL_NONE, default is GL_BACK
+	//  - InvertY: If true, the texture will be flipped vertically, default is false
 	static std::vector<Materiel> fromFile(ResourceManager* rm, const std::string& filename);
 	const static GLenum DefaultCullMode = GL_BACK;
 
